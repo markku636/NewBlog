70fc37a65c577dede0a70a0b295b1268e9815b25
Mark.K <a4756830@gmail.com>
2022-08-31 23:19:19 +0800

    update

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
       resolve: '@elegantstack/gatsby-theme-flexiblog-personal',
       options: {        
         sources: {
-          local: false
+          local: true
         }
       }
     }
