99f2e0adb959ffb1b14b6f55093105302a4025a1
Mark.K <a4756830@gmail.com>
2022-08-31 23:08:35 +0800

    update

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -4,13 +4,19 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {}
+    },
+    {
+      // ATTENTION: Match the theme name with the theme you're using
       resolve: '@elegantstack/gatsby-theme-flexiblog-personal',
-      options: {
-        // Add theme options here. Check documentation for available options.
-        siteUrl: process.env.URL || process.env.VERCEL_URL
+      options: {        
+        sources: {
+          local: true
+        }
       }
     }
-  ],
+  ], 
   // Customize your site metadata:
   siteMetadata: {
     //General Site Metadata
