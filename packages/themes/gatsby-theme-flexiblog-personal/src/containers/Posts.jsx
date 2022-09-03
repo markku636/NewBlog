f7ce326dc91264080175f10d57b1176e9dbaa5e1
Mark.K <a4756830@gmail.com>
2022-09-04 00:22:12 +0800

    update

--- packages/themes/gatsby-theme-flexiblog-personal/src/containers/Posts.jsx
+++ packages/themes/gatsby-theme-flexiblog-personal/src/containers/Posts.jsx
@@ -24,7 +24,7 @@ const Posts = ({ data: { paginatedPosts = {} }, ...props }) => {
 
   return (
     <Layout {...props}>
-      <Seo title='擁有豐富網站開發經驗，直播系統、POS系統、電子商務、平台網站、SEO，專業的網站開發鐵三工程師。' />
+      <Seo title='擁有豐富網站開發經驗，直播系統、POS系統、電子商務、平台網站、SEO，專業的網站開發鐵三工程師。' description="Hi, I’m Mark.Ku. 我是一位網頁軟體工程師，喜歡研究各種技術的解決方案，並用於實踐商業上、興趣是鐵人三項及登山、攝影，這個BLOG主要分享工作及生活的點滴。" />
       <Hero sx={{ bg: `contentBg` }}>
         <HeroComponent {...props} />
       </Hero>
