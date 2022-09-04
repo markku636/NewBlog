7782934d4d84877ea1e34d23005aa94bf819d9a1
Mark.K <a4756830@gmail.com>
2022-09-04 09:14:02 +0800

    update

--- packages/flow-ui/flow-ui-layout/src/Header/Header.Menu.jsx
+++ packages/flow-ui/flow-ui-layout/src/Header/Header.Menu.jsx
@@ -3,6 +3,8 @@ import { Box } from 'theme-ui'
 import Navigation from '@components/Navigation'
 import Drawer from '@components/Drawer'
 import useSiteMetadata from '@helpers-blog/useSiteMetadata'
+import { useAllPosts } from '@helpers-blog'
+
 
 const styles = {
   desktopMenu: {
@@ -18,6 +20,20 @@ const styles = {
 
 export const HeaderMenu = ({ mobileMenu = {} }) => {
   const { headerMenu } = useSiteMetadata()
+
+  const allPost = useAllPosts()
+
+  if (mobileMenu && mobileMenu.items) {
+    mobileMenu.items = mobileMenu.items.map(function (
+      menu
+    ) {
+      menu.totalCount = allPost?.filter(
+        z => z.category && z.category?.name === menu.name
+      ).length
+  
+      return menu
+    })
+  }
 
   const desktopMenuNav = (
     <Navigation
--- packages/flow-ui/flow-ui-layout/src/Layout/Layout.jsx
+++ packages/flow-ui/flow-ui-layout/src/Layout/Layout.jsx
@@ -5,52 +5,26 @@ import theme from '@elegantstack/flow-ui-theme/src/theme'
 import pageContextProvider from '@helpers/pageContextProvider'
 import { Header } from '../Header/Header'
 import { Footer } from '../Footer/Footer'
-import { useAllPosts } from '@helpers-blog'
 
-export const Layout = ({ children, pageContext, location }) => {
-  pageContext.mobileMenu
+export const Layout = ({ children, pageContext, location }) => (
+  <ThemeProvider theme={theme}>    
+    <pageContextProvider.Provider value={{ pageContext, location }}>
+    
+  <link href="https://cdn.jsdelivr.net/npm/taipei-sans-tc" rel="stylesheet" />
+  <link
+  href="https://cdn.jsdelivr.net/npm/taipei-sans-tc/dist/Light/TaipeiSansTCBeta-Light.css"
+  rel="stylesheet"/>
+ <link href="https://cdn.jsdelivr.net/npm/taipei-sans-tc/dist/Bold/TaipeiSansTCBeta-Bold.css"
+  rel="stylesheet"/>  
+  
+  <script id="first-unique-id" dangerouslySetInnerHTML={{ __html: `window.$crisp=[];window.CRISP_WEBSITE_ID="04b00852-a5b2-4c94-b2d1-1f432ce3eac0";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();` }} />
 
-  const allPost = useAllPosts()
-  pageContext.mobileMenu.items = pageContext.mobileMenu.items.map(function (
-    menu
-  ) {
-    menu.totalCount = allPost?.filter(
-      z => z.category && z.category?.name === menu.name
-    ).length
-
-    return menu
-  })
-
-  return (
-    <ThemeProvider theme={theme}>
-      <pageContextProvider.Provider value={{ pageContext, location }}>
-        <link
-          href='https://cdn.jsdelivr.net/npm/taipei-sans-tc'
-          rel='stylesheet'
-        />
-        <link
-          href='https://cdn.jsdelivr.net/npm/taipei-sans-tc/dist/Light/TaipeiSansTCBeta-Light.css'
-          rel='stylesheet'
-        />
-        <link
-          href='https://cdn.jsdelivr.net/npm/taipei-sans-tc/dist/Bold/TaipeiSansTCBeta-Bold.css'
-          rel='stylesheet'
-        />
-
-        <script
-          id='first-unique-id'
-          dangerouslySetInnerHTML={{
-            __html: `window.$crisp=[];window.CRISP_WEBSITE_ID="04b00852-a5b2-4c94-b2d1-1f432ce3eac0";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`
-          }}
-        />
-
-        <Flex variant='layout.layout'>
-          <Global styles={css(theme => theme.global)} />
-          <Header />
-          <Box variant='layout.body'>{children}</Box>
-          <Footer />
-        </Flex>
-      </pageContextProvider.Provider>
-    </ThemeProvider>
-  )
-}
+      <Flex variant='layout.layout'>
+        <Global styles={css(theme => theme.global)} />
+        <Header />
+        <Box variant='layout.body'>{children}</Box>
+        <Footer />
+      </Flex>
+    </pageContextProvider.Provider>
+  </ThemeProvider>
+)
