405185f7cfca044396ff47a439a44fe0857be4e3
Mark.K <a4756830@gmail.com>
2022-09-04 21:52:45 +0800

    update

--- packages/flow-ui/flow-ui-components/src/Card/Card.Media.Image.jsx
+++ packages/flow-ui/flow-ui-components/src/Card/Card.Media.Image.jsx
@@ -3,22 +3,30 @@ import { GatsbyImage as Img } from 'gatsby-plugin-image'
 import { css } from 'theme-ui'
 import rv from '@components/utils/buildResponsiveVariant'
 import { isMobile } from '../../../../utils/index'
-const CardMediaImage = ({ variant, loading, image, title }) => (
-  <Img
-    backgroundColor={isMobile ? '#FFFFFF' : ''}
-    image={image}
-    loading={loading}
-    alt={title|| 'title'}
-    css={css({
-      height: `full`,
-      verticalAlign: `middle`, //avoid baseline gap
-      img: {
-        bg: `omegaLighter`
-      },
-      variant: rv(variant, 'image')
-    })}
-  />
-)
+
+import { useColorMode } from 'theme-ui'
+
+const CardMediaImage = ({ variant, loading, image, title }) => {
+  const [colorMode] = useColorMode()
+  const isDark = colorMode === `dark`
+  return (
+    <Img
+      backgroundColor={isMobile && !isDark ? '#FFFFFF' : ''}
+      image={image}
+      loading={loading}
+      alt={title || 'title'}
+      css={css({
+        height: `full`,
+        verticalAlign: `middle`, //avoid baseline gap
+        img: {
+          bg: `omegaLighter`
+        },
+        variant: rv(variant, 'image')
+      })}
+    />
+  )
+}
+
 CardMediaImage.defaultProps = {
   loading: 'lazy'
 }
