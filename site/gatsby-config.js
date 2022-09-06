059a0a14e093f8a6e01242eca2b45db0d53f01a7
Mark.K <a4756830@gmail.com>
2022-09-06 23:03:13 +0800

    update

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -7,9 +7,19 @@ module.exports = {
     DEV_SSR: false
   },
   plugins: [
-    // 加入下面兩行
-    'gatsby-redirect-from',
-    'gatsby-plugin-meta-redirect', // make sure this is always the last one
+    {
+      resolve: `gatsby-plugin-gatsby-cloud`,
+      options: {
+        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        generateMatchPathRewrites: true // boolean to turn off automatic creation of redirect rules for client only paths
+      }
+    },
+
     {
       resolve: `gatsby-transformer-remark`,
       options: {
@@ -22,7 +32,7 @@ module.exports = {
       }
     },
     // ...
-    
+
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
