267c6dbdad46ff087484356f5c73bd3b89c696f9
Mark.K <a4756830@gmail.com>
2023-04-20 20:19:29 +0800

    update

--- packages/flow-ui/flow-ui-layout/src/Layout/Layout.jsx
+++ packages/flow-ui/flow-ui-layout/src/Layout/Layout.jsx
@@ -7,6 +7,26 @@ import { Header } from '../Header/Header'
 import { Footer } from '../Footer/Footer'
 import { Helmet } from 'react-helmet'
 
+export const personSchema = () => {
+  const schema = {
+    '@context': 'https://schema.org/',
+    '@type': 'Person',
+    name: 'Mark ku',
+    url: 'https://blog.markkulab.net/',
+    image:
+      'https://blog.markkulab.net/static/cbb689972a03a9278c89aab9e19e2d6f/6c33c/hero.webp',
+    sameAs: 'https://www.letgo.com.tw',
+    jobTitle: 'Senior Softwarer Developer'
+  }
+
+  return (
+    <script
+      type='application/ld+json'
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+    ></script>
+  )
+}
+
 export const Layout = ({ children, pageContext, location }) => (
   <ThemeProvider theme={theme}>
     <pageContextProvider.Provider value={{ pageContext, location }}>
@@ -25,19 +45,7 @@ export const Layout = ({ children, pageContext, location }) => (
 
       <script src='https://assets.salesmartly.com/js/project_14754_15283_1681818583.js'></script>
       <link rel='canonical' href='http://https://blog.markkulab.net/' />
-      <script type='application/ld+json'>
-        {{
-          '@context': 'https://schema.org/',
-          '@type': 'Person',
-          name: 'Mark ku',
-          url: 'https://blog.markkulab.net/',
-          image:
-            'https://blog.markkulab.net/static/cbb689972a03a9278c89aab9e19e2d6f/6c33c/hero.webp',
-          sameAs: 'https://www.letgo.com.tw',
-          jobTitle: 'Senior Softwarer Developer'
-        }}
-      </script>
-
+      {personSchema}
       <Flex variant='layout.layout'>
         <Global styles={css(theme => theme.global)} />
         <Header />
