eef3426eb79b49c2d1099622dde8152066e3c49c
Mark.K <a4756830@gmail.com>
2023-04-20 19:50:00 +0800

    update

--- packages/flow-ui/flow-ui-layout/src/Layout/Layout.jsx
+++ packages/flow-ui/flow-ui-layout/src/Layout/Layout.jsx
@@ -23,7 +23,20 @@ export const Layout = ({ children, pageContext, location }) => (
         rel='stylesheet'
       />
 
-      <script src="https://assets.salesmartly.com/js/project_14754_15283_1681818583.js"></script>
+      <script src='https://assets.salesmartly.com/js/project_14754_15283_1681818583.js'></script>
+      <link rel='canonical' href='http://https://blog.markkulab.net/' />
+      <script type='application/ld+json'>
+        {{
+          '@context': 'https://schema.org/',
+          '@type': 'Person',
+          name: 'Mark ku',
+          url: 'https://blog.markkulab.net/',
+          image:
+            'https://blog.markkulab.net/static/cbb689972a03a9278c89aab9e19e2d6f/6c33c/hero.webp',
+          sameAs: 'https://www.letgo.com.tw',
+          jobTitle: 'Senior Softwarer Developer'
+        }}
+      </script>
 
       <Flex variant='layout.layout'>
         <Global styles={css(theme => theme.global)} />
