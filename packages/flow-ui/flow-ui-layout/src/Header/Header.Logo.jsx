b8603bb3d0e1b6f0a022f52d7c18dda2104b2a38
mark.ku <mark.ku@ibuypower.com>
2024-02-19 22:43:34 +0800

    update

--- packages/flow-ui/flow-ui-layout/src/Header/Header.Logo.jsx
+++ packages/flow-ui/flow-ui-layout/src/Header/Header.Logo.jsx
@@ -33,7 +33,7 @@ const logoQuery = graphql`
     ) {
       childImageSharp {
         gatsbyImageData(
-          width: 150
+          width: 232
           layout: CONSTRAINED
           quality: 100
           placeholder: NONE
@@ -46,7 +46,7 @@ const logoQuery = graphql`
     ) {
       childImageSharp {
         gatsbyImageData(
-          width: 150
+          width: 232
           layout: CONSTRAINED
           quality: 100
           placeholder: NONE
