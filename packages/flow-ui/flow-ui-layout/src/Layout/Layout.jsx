50c1be36def8f79d4c67beb159da31ab54018378
mark.ku <a4756830@gmail.com>
2025-07-28 12:23:30 +0800

    update scripts

--- packages/flow-ui/flow-ui-layout/src/Layout/Layout.jsx
+++ packages/flow-ui/flow-ui-layout/src/Layout/Layout.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import { Global } from '@emotion/core'
-import { ThemeProvider, Flex, Box, css } from 'theme-ui'
 import theme from '@elegantstack/flow-ui-theme/src/theme'
+import { Global } from '@emotion/core'
 import pageContextProvider from '@helpers/pageContextProvider'
-import { Header } from '../Header/Header'
+import React from 'react'
+import { Box, css, Flex, ThemeProvider } from 'theme-ui'
 import { Footer } from '../Footer/Footer'
-import { Helmet } from 'react-helmet'
+import { Header } from '../Header/Header'
 
 export const personSchema = () => {
   const schema = {
@@ -45,14 +44,19 @@ export const Layout = ({ children, pageContext, location }) => (
       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2396931274355535"
      crossorigin="anonymous"></script>
 
-      <script type="text/javascript">
-      {`(function(w, d, s, u) {
-      w.RocketChat = function(c) { w.RocketChat._.push(c) }; w.RocketChat._ = []; w.RocketChat.url = u;
-      var h = d.getElementsByTagName(s)[0], j = d.createElement(s);
-      j.async = true; j.src = 'https://chat.226network.com/livechat/rocketchat-livechat.min.js?_=201903270000';
-      h.parentNode.insertBefore(j, h);
-      })(window, document, 'script', 'https://chat.226network.com/livechat');`}
-      </script>
+      <script 
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w, d, s, u) {
+              w.RocketChat = function(c) { w.RocketChat._.push(c) }; w.RocketChat._ = []; w.RocketChat.url = u;
+              var h = d.getElementsByTagName(s)[0], j = d.createElement(s);
+              j.async = true; j.src = 'https://chat.226network.com/livechat/rocketchat-livechat.min.js?_=201903270000';
+              h.parentNode.insertBefore(j, h);
+            })(window, document, 'script', 'https://chat.226network.com/livechat');
+          `
+        }}
+      />
 
       <link rel='canonical' href='http://https://blog.markkulab.net/' />
       {personSchema()}
